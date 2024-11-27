379849e027315bb00a0ca70ad4b0b8534990d4d4
KendryAR <kendry.bc2.2001@gmail.com>
2024-11-27 12:12:08 -0500

    Agrega M.POST para Genero

--- Biblioteca/Controllers/GeneroController.cs
+++ Biblioteca/Controllers/GeneroController.cs
@@ -1,4 +1,5 @@
 ﻿using Biblioteca.Contexts;
+using Biblioteca.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,26 @@ namespace Biblioteca.Controllers
         {
             var generos=await _context.Generos.ToListAsync();
             return Ok(generos);
+        }
+
+        [HttpPost("crear-Genero")]
+        public async Task<IActionResult> CrearGenero([FromBody] Genero nuevoGenero)
+        {
+            if (nuevoGenero == null)
+            {
+                return BadRequest("El género no puede ser nulo.");
+            }
+
+            nuevoGenero.GeneroID = null;
+
+            // Agregar el nuevo género al contexto de la base de datos
+            await _context.Generos.AddAsync(nuevoGenero);
+
+            // Guardar los cambios en la base de datos
+            await _context.SaveChangesAsync();
+
+            // Retornar una respuesta con el objeto creado, y el código de estado 201 (Creado)
+            return CreatedAtAction(nameof(CrearGenero), new { id = nuevoGenero.GeneroID }, nuevoGenero);
         }
 
 
